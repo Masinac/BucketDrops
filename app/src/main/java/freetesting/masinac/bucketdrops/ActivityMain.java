a74340d3b1e6609f65c94f0736034792b24b6485
masinac <miki1401@gmail.com>
2016-10-19 23:33:48 +0200

    Added onClick listener on "Add a Drop" btn

--- app/src/main/java/freetesting/masinac/bucketdrops/ActivityMain.java
+++ app/src/main/java/freetesting/masinac/bucketdrops/ActivityMain.java
@@ -3,13 +3,17 @@ package freetesting.masinac.bucketdrops;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener {
 
     Toolbar toolbar;
+    Button addDropButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,13 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        addDropButton = (Button) findViewById(R.id.btn_add_drop);
+        addDropButton.setOnClickListener(this);
+
         setSupportActionBar(toolbar);
         initBackgroundImage();
+
+
     }
 
     private void initBackgroundImage() {
@@ -27,5 +36,10 @@ public class ActivityMain extends AppCompatActivity {
         if (backgroundImageView != null) {
             Glide.with(this).load(R.drawable.background).centerCrop().into(backgroundImageView);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "my prefered method", Toast.LENGTH_SHORT).show();
     }
 }
