ab621ab5ea20668a779d13b8819b5e2e38159653
masinac <miki1401@gmail.com>
2016-10-25 22:46:09 +0200

    Integrated Realm database and added Stetho that is used for debugging

--- app/src/main/java/freetesting/masinac/bucketdrops/ActivityMain.java
+++ app/src/main/java/freetesting/masinac/bucketdrops/ActivityMain.java
@@ -6,9 +6,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
 
 public class ActivityMain extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +21,14 @@ public class ActivityMain extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Realm.init(this);
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,9 +37,9 @@ public class ActivityMain extends AppCompatActivity implements View.OnClickListe
 
         setSupportActionBar(toolbar);
         initBackgroundImage();
-
-
     }
+
+
 
     private void initBackgroundImage() {
         ImageView backgroundImageView = (ImageView) findViewById(R.id.background);
--- app/src/main/java/freetesting/masinac/bucketdrops/DialogAdd.java
+++ app/src/main/java/freetesting/masinac/bucketdrops/DialogAdd.java
@@ -11,6 +11,10 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import freetesting.masinac.bucketdrops.beans.Drop;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Created by masinac on 10/20/16.
  */
@@ -25,9 +29,30 @@ public class DialogAdd extends DialogFragment {
     private View.OnClickListener closeDialogClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            int id = v.getId();
+            switch (id) {
+                case R.id.dialog_add_button : addAction(); break;
+            }
+
             dismiss();
         }
     };
+
+    private void addAction() {
+        String inputText = dropInput.getText().toString();
+        long currentTime = System.currentTimeMillis();
+
+        Drop drop = new Drop(inputText, currentTime, 0, false); //will integrate end date later
+
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+
+        Realm.setDefaultConfiguration(realmConfiguration);
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        realm.copyToRealm(drop);
+        realm.commitTransaction();
+        realm.close();
+    }
 
 
     public DialogAdd() {
@@ -43,12 +68,15 @@ public class DialogAdd extends DialogFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        addDrop = (Button) view.findViewById(R.id.btn_add_drop);
+        addDrop = (Button) view.findViewById(R.id.dialog_add_button);
         closeDialog = (ImageButton) view.findViewById(R.id.dialog_close_button);
         dropInput = (EditText) view.findViewById(R.id.dialog_drop_text);
         datePicker = (DatePicker) view.findViewById(R.id.dialog_date_picker);
 
         closeDialog.setOnClickListener(closeDialogClickListener);
+        addDrop.setOnClickListener(closeDialogClickListener);
 
     }
+
+
 }
